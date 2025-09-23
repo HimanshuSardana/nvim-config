f8b6b2a5d4f2179f06ef4836d7169823338721f4
Himanshu Sardana <himanshusardana2005@gmail.com>
2025-09-23 22:27:30 +0530

    chore: add fzf-lua select UI

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -3,8 +3,7 @@ return {
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	opts = {},
 	config = function()
-		require('fzf-lua').setup({
-			"ivy"
-		})
+		require('fzf-lua').setup({ "ivy" })
+		require('fzf-lua').register_ui_select()
 	end
 }
